80a19006e68994758eb994e2a6c8de1cd56ddea8
renatomarcio18081971 <renato.marcio@gmail.com>
2020-04-13 17:25:45 -0300

    subindo o resto

--- Angular/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ Angular/gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TarefaService } from './shared';
-
+import { TarefaService, TarefaConcluidaDirective } from './shared';
+import { ListarTarefaComponent } from './listar/listar-tarefa.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CadastrarTarefaComponent } from './cadastrar/cadastrar-tarefa.component';
+import { EditarTarefaComponent } from './editar/editar-tarefa.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent,
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
